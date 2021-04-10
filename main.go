687aac72a8251e0ad1f22c57ec68607a9186ddff
bartosz.lenart <lenart.consulting@gmail.com>
2021-04-10 13:59:24 +0200

    add terminal information prints for initialization process

--- main.go
+++ main.go
@@ -16,6 +16,8 @@ import (
 	"github.com/pterm/pterm"
 )
 
+var srvAddressAndPort = "0.0.0.0:8000"
+
 // FirebaseTranslations relation input to translations
 type FirebaseTranslations struct {
 	Input      string       `json:"input"`
@@ -35,8 +37,7 @@ func mapToLanguage(ft []FirebaseTranslations) *map[string]map[string]string {
 	defer progressbar.Stop()
 	languages := make(map[string]map[string]string)
 	for i, group := range ft {
-		progressbar.Title = fmt.Sprintf("Adding %v translation", i)
-		pterm.Success.Printf("Translation %v added", i)
+		pterm.DefaultCenter.Println(pterm.Success.Sprintf("Translation %v added", i))
 		progressbar.Increment()
 		for ln, trans := range group.Translated {
 			if l, ok := languages[ln]; ok {
@@ -52,6 +53,26 @@ func mapToLanguage(ft []FirebaseTranslations) *map[string]map[string]string {
 		}
 	}
 	return &languages
+}
+
+func renderEndpoints(ct *map[string]map[string]string, addr string) {
+
+	data := pterm.TableData{
+		{"language code", "endpoint"},
+	}
+
+	for k := range *ct {
+		data = append(data, []string{k, fmt.Sprintf("%s/translations/%s", addr, k)})
+	}
+
+
+	s, err := pterm.DefaultTable.WithHasHeader().WithData(data).Srender()
+	if err != nil {
+		log.Fatalf("cannot log endpoints table, error: %s", err)
+	}
+	header := pterm.DefaultHeader.WithBackgroundStyle(pterm.NewStyle(pterm.BgRed))
+	pterm.DefaultCenter.Println(header.Sprint("Translation endpoints list:"))
+	pterm.DefaultCenter.Println(s)
 }
 
 func getTranslations(ctx iris.Context) {
@@ -74,15 +95,17 @@ func getTranslations(ctx iris.Context) {
 
 func init() {
 
-	header := pterm.DefaultHeader.WithBackgroundStyle(pterm.NewStyle(pterm.BgRed))
-	pterm.DefaultCenter.Println(header.Sprint("Foodie Translations Micro-service"))
-
-	_ = pterm.DefaultBigText.WithLetters(pterm.NewLettersFromString("Lan_go quick")).Render()
+	s, err := pterm.DefaultBigText.WithLetters(pterm.NewLettersFromString("Lan.go quick")).Srender()
+	if err != nil {
+		log.Fatalf("cannot log in BIG TEXT, error: %s", err)
+	}
+	pterm.DefaultCenter.Println(s)
 
 	godotenv.Load()
 	fBToken := os.Getenv("FIREBASE_FUNC_TOKEN")
 	fBTranslationAddress := os.Getenv("FIREBASE_FUNC_ADDRESS")
 	fBUserEmail := os.Getenv("FIREBASE_USER_EMAIL_ADDRESS")
+	srvAddressAndPort = os.Getenv("SERVER_ADDRESS_AND_PORT")
 
 	postBody, err := json.Marshal(map[string]string{
 		"email": fBUserEmail,
@@ -125,13 +148,11 @@ func init() {
 	atomicRWTranslations.mu.Lock()
 	defer atomicRWTranslations.mu.Unlock()
 	atomicRWTranslations.ct = mapToLanguage(*translations)
-
-	pterm.DefaultSection.Printf("properly initialized lango quick microservice with %v languages", len(*atomicRWTranslations.ct))
+	pterm.DefaultCenter.Println(pterm.DefaultBasicText.Sprintf("properly initialized lango quick microservice with %v languages", len(*atomicRWTranslations.ct)))
+	renderEndpoints(atomicRWTranslations.ct, srvAddressAndPort)
 }
 
 func main() {
-	srvAddressAndPort := os.Getenv("SERVER_ADDRESS_AND_PORT")
-
 	app := iris.New()
   	app.Handle("GET", "/translations/{ln:string}", getTranslations)
   	app.Listen(srvAddressAndPort)
