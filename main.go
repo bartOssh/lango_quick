dcf9601722a06e6c05ec0c27a876f12fa60d7457
bartosz.lenart <lenart.consulting@gmail.com>
2021-04-11 15:00:50 +0200

    post translation to firebase, get translations from firebase after successfully created

--- main.go
+++ main.go
@@ -16,8 +16,6 @@ import (
 	"github.com/pterm/pterm"
 )
 
-var srvAddressAndPort = "0.0.0.0:8000"
-
 // firebaseTranslations relation input to translations
 type firebaseTranslations struct {
 	Input      string       `json:"input"`
@@ -29,10 +27,25 @@ type requestTranslations struct {
 	Translate []string `json:"translate"`
 }
 
+type langoClient struct {
+	inner *http.Client
+	fBToken, fBTranslationAddress, fBUserEmail string
+}
+
+// serverAddressAndPort is a default address and port,
+// if SERVER_ADDRESS_AND_PORT env variable is provided then variable is overwritten
+var srvAddressAndPort = "0.0.0.0:8000"
+
+var sleepTime time.Duration = 60
+
 var atomicRWTranslations struct {
 	mu sync.RWMutex
 	ct *map[string]map[string]string
 }
+
+// global for this service http client
+var client langoClient
+
 
 // mapToLanguage iterate through input : translations slice and maps translations per language key like so:
 // {"language_key" : {"translation in english as a key" : "translation to language of the language_key"}}
@@ -106,43 +119,32 @@ func createTranslations(ctx iris.Context) {
 	if err != nil {
 		ctx.StatusCode(iris.StatusBadRequest)
 	}
-	fmt.Printf("TRANSLATIONS: \n %v \n", t)
+	pterm.DefaultCenter.Println(pterm.Info.Sprintf("Sending request to remote firebase functions to create %v translations", len(t.Translate)))
+	if ok := client.postCreateToRemote(t.Translate); !ok {
+		pterm.DefaultCenter.Println("Error received from remote firebase function, couldn't create translations")
+		ctx.StatusCode(iris.StatusInternalServerError)
+		return
+	}
+	pterm.DefaultCenter.Println(pterm.Success.Sprintf("Created %v translations", len(t.Translate)))
+	go func() {
+		time.Sleep(sleepTime*time.Second)
+		client.readTranslationsFromRemote()
+	}()
 	ctx.StatusCode(iris.StatusAccepted)
 }
 
-func init() {
-
-	s, err := pterm.DefaultBigText.WithLetters(pterm.NewLettersFromString("Lan.go quick")).Srender()
-	if err != nil {
-		log.Fatalf("cannot log in BIG TEXT, error: %s", err)
-	}
-	pterm.DefaultCenter.Println(s)
-
-	godotenv.Load()
-	fBToken := os.Getenv("FIREBASE_FUNC_TOKEN")
-	fBTranslationAddress := os.Getenv("FIREBASE_FUNC_ADDRESS")
-	fBUserEmail := os.Getenv("FIREBASE_USER_EMAIL_ADDRESS")
-	srvAddressAndPort = os.Getenv("SERVER_ADDRESS_AND_PORT")
-
+func (c langoClient) readTranslationsFromRemote() {
 	postBody, err := json.Marshal(map[string]string{
-		"email": fBUserEmail,
-		"token": fBToken,
+		"email": c.fBUserEmail,
+		"token": c.fBToken,
 	})
 	if err != nil {
 		log.Fatalf("cannot initialize post body, error: %s", err)
 	}
-
-	tr := &http.Transport{
-		MaxIdleConns:       MaxIdleConn,
-		IdleConnTimeout:    IdleConnTimeout * time.Second,
-		DisableCompression: true,
-	}
-	client := &http.Client{Transport: tr}
-	
 	rawBody := bytes.NewBuffer(postBody)
-	resp, err := client.Post(fBTranslationAddress, "application/json", rawBody)
+	resp, err := c.inner.Post(c.fBTranslationAddress + "/translations", "application/json", rawBody)
 	if err != nil {
-		log.Fatalf("cannot fetch initial translation value, error: %s", err)
+		log.Fatalf("error when posting translation request to remote firebase function, error: %s", err)
 	}
 
 	defer resp.Body.Close()
@@ -160,13 +162,63 @@ func init() {
 		}
 		log.Fatalf("cennot unmarshal firebase translation result %s for response %s", err, result)
 	}
-
-	atomicRWTranslations.mu = sync.RWMutex{}
 	atomicRWTranslations.mu.Lock()
 	defer atomicRWTranslations.mu.Unlock()
 	atomicRWTranslations.ct = mapToLanguage(*translations)
 	pterm.DefaultCenter.Println(pterm.DefaultBasicText.Sprintf("properly initialized lango quick microservice with %v languages", len(*atomicRWTranslations.ct)))
 	renderEndpoints(atomicRWTranslations.ct, srvAddressAndPort)
+}
+
+func (c langoClient) postCreateToRemote(data []string) (ok bool) {
+	payload := struct{
+		Email string `json:"email"`
+		Token string `json:"token"`
+		Translations []string `json:"translations"`
+	}{
+		Email: c.fBUserEmail,
+		Token: c.fBToken,
+		Translations: data,	
+	}
+	postBody, err := json.Marshal(payload)
+	if err != nil {
+		log.Fatalf("cannot initialize post body, error: %s", err)
+	}
+	rawBody := bytes.NewBuffer(postBody)
+	resp, err := c.inner.Post(c.fBTranslationAddress + "/create", "application/json", rawBody)
+	if err != nil {
+		log.Fatalf("error when posting create request to remote firebase function, error: %s", err)
+	}
+	if resp.StatusCode !=  http.StatusCreated {
+		return
+	}
+	ok = true
+	return
+}
+
+func init() {
+
+	s, err := pterm.DefaultBigText.WithLetters(pterm.NewLettersFromString("Lan.go quick")).Srender()
+	if err != nil {
+		log.Fatalf("cannot log in BIG TEXT, error: %s", err)
+	}
+	pterm.DefaultCenter.Println(s)
+
+	godotenv.Load()
+	tr := &http.Transport{
+		MaxIdleConns:       MaxIdleConn,
+		IdleConnTimeout:    IdleConnTimeout * time.Second,
+		DisableCompression: true,
+	}
+	client = langoClient{inner: &http.Client{Transport: tr}}
+
+	client.fBToken = os.Getenv("FIREBASE_FUNC_TOKEN")
+	client.fBTranslationAddress = os.Getenv("FIREBASE_FUNC_ADDRESS")
+	client.fBUserEmail = os.Getenv("FIREBASE_USER_EMAIL_ADDRESS")
+	srvAddressAndPort = os.Getenv("SERVER_ADDRESS_AND_PORT")
+
+	atomicRWTranslations.mu = sync.RWMutex{}
+
+	client.readTranslationsFromRemote()
 }
 
 func main() {
